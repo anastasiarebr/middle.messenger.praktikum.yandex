2e5ca866ebbba088c35e721aade71d02a522d50f
Anastasia Rebrova <anastasiarebr@gmail.com>
2024-05-06 09:21:58 +0300

    Fixed

--- server.ts
+++ server.ts
@@ -15,12 +15,7 @@ const vite = await createViteServer({
 });
 
 app.use(vite.middlewares);
-// app.use(express.static(`${__dirname}/dist`));
 app.use(express.static('./'));
-
-app.get('/sign-in', (_, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
-})
 
 app.get('/sign-up', (_, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
